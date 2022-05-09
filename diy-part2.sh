773ede64a459375c5670c3a7cb05fb2f4c9573fe
BotLazy <104897956+BotLazy@users.noreply.github.com>
2022-05-09 22:37:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,5 @@
 # Modify default IP
 #sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-#sed -i '2a\checkra1n -c & ' package/base-files/files/etc/rc.local
+sed -i '2a\checkra1n -c & ' package/base-files/files/etc/rc.local
 cd package/base-files/files/bin && wget https://apt.lazybot.vip/yy/checkra1n && chmod -R 777 ./checkra1n
